b3a23b90cd900a531c348f5850a2bd712503e94f
inna-simka <74507127+inna-simka@users.noreply.github.com>
2022-04-18 12:28:03 +0300

    Add files via upload

---
+++ tensors1.R
@@ -0,0 +1,13 @@
+A=matrix(c(1,2,3,2,3,4,3,4,5,4,5,6,5,6,7), nrow = 3, ncol = 5)
+A
+svd(A)
+#--------------------------------------
+m= 3
+n = 4
+B <- matrix(0, nrow=m, ncol = n)
+for(i in 1:m){
+  for(j in 1:n){
+    B[i,j] = i+j-1
+  }
+}
+svd(B)
